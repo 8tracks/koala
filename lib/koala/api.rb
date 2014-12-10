03d798ea908fd5d8faca92b7d8db92b64cbe71fc
Netto Farah <nettofarah@gmail.com>
2014-12-09 17:37:07 -0800

    tries to unzip response before parsing it

--- lib/koala/api.rb
+++ lib/koala/api.rb
@@ -82,7 +82,14 @@ module Koala
           # parse the body as JSON and run it through the error checker (if provided)
           # Note: Facebook sometimes sends results like "true" and "false", which aren't strictly objects
           # and cause MultiJson.load to fail -- so we account for that by wrapping the result in []
-          MultiJson.load("[#{result.body.to_s}]")[0]
+
+          begin
+            gz = Zlib::GzipReader.new(StringIO.new(result.body.to_s))
+            tempjson = gz.read
+            MultiJson.load(tempjson)
+          rescue Zlib::GzipFile::Error => e
+            return MultiJson.load("#{result.body.to_s}")
+          end
         end
       end
 
